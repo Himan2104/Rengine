24bf8af66789cc5914df3c799f2b053293270862
Himanshu Parchand <himan2104@outlook.com>
2025-04-06 16:54:00 +0530

    Decoupled Core and AF

--- Engine/Include/Rengine/ECS/Entity.hpp
+++ Engine/Include/Rengine/ECS/Entity.hpp
@@ -1,41 +1,56 @@
 #pragma once
-#include<stdint.h>
-#include<string>
-#include<vector>
-#include<memory>
 
-#include"Rengine/Config.hpp"
-#include"Rengine/ECS/Component.hpp"
+#include "Rengine/Core/Definitions.hpp"
 
-namespace Ren::ecs
+namespace Ren::ECS
 {
-	class Component;
 
-	class Entity
-	{
-	public:
-		Entity();
-		Entity(uint_fast64_t instanceID);
-		~Entity();
-		
-		enum class State 
-		{
-			Active,		// Entity is active
-			Inactive,	// Entity is inactive
-			Destroyed	// Entity is destroyed (will free up memory when all references are destroyed)
-		};
+enum class EntityFlags : UInt32
+{
+    EntityFlagPlaceholder_0  = 1u << 0,
+    EntityFlagPlaceholder_1  = 1u << 1,
+    EntityFlagPlaceholder_2  = 1u << 2,
+    EntityFlagPlaceholder_3  = 1u << 3,
+    EntityFlagPlaceholder_4  = 1u << 4,
+    EntityFlagPlaceholder_5  = 1u << 5,
+    EntityFlagPlaceholder_6  = 1u << 6,
+    EntityFlagPlaceholder_7  = 1u << 7,
+    EntityFlagPlaceholder_8  = 1u << 8,
+    EntityFlagPlaceholder_9  = 1u << 9,
+    EntityFlagPlaceholder_10 = 1u << 10,
+    EntityFlagPlaceholder_11 = 1u << 11,
+    EntityFlagPlaceholder_12 = 1u << 12,
+    EntityFlagPlaceholder_13 = 1u << 13,
+    EntityFlagPlaceholder_14 = 1u << 14,
+    EntityFlagPlaceholder_15 = 1u << 15,
+    EntityFlagPlaceholder_16 = 1u << 16,
+    EntityFlagPlaceholder_17 = 1u << 17,
+    EntityFlagPlaceholder_18 = 1u << 18,
+    EntityFlagPlaceholder_19 = 1u << 19,
+    EntityFlagPlaceholder_20 = 1u << 20,
+    EntityFlagPlaceholder_21 = 1u << 21,
+    EntityFlagPlaceholder_22 = 1u << 22,
+    EntityFlagPlaceholder_23 = 1u << 23,
+    EntityFlagPlaceholder_24 = 1u << 24,
+    EntityFlagPlaceholder_25 = 1u << 25,
+    EntityFlagPlaceholder_26 = 1u << 26,
+    EntityFlagPlaceholder_27 = 1u << 27,
+    EntityFlagPlaceholder_28 = 1u << 28,
+    EntityFlagPlaceholder_29 = 1u << 29,
+    EntityFlagPlaceholder_30 = 1u << 30,
+    EntityFlagPlaceholder_31 = 1u << 31
+};
 
-		std::string  identifier;
+class Entity
+{
+public:
+    Entity() = default;
 
-		uint_fast64_t getInstanceID();
+    template <typename T> class IComponent* GetComponent();
 
-	private:
-		uint_fast64_t _instance_id;
-		State _state;
-		std::vector<std::shared_ptr<Ren::ecs::Component>> _components;
+private:
+    UInt32 _entityID;
+    EntityFlags _entityFlags;
+};
 
-		friend class EntityManager;
-	};
-
-}
-
+} // namespace Ren::ECS
--- Engine/Include/Rengine/Utility/Misc.hpp
+++ Engine/Include/Rengine/Utility/Misc.hpp
@@ -1,51 +1,41 @@
 #pragma once
-#include"Rengine/Config.hpp"
-#include<string>
+#include <string>
 
 namespace Ren
 {
-    
 
-    /// <summary>
-    /// Get the current date and time as a string
-    /// </summary>
-    /// <param name="format"> Format string, default is [YYYY-MM-DD|HH:MM:SS]</param>
-    /// <returns>String of current date and time as the specified format</returns>
-    std::string GetCurrentTime(const char* format = "[%Y-%m-%d|%X]");
-    
+/// <summary>
+/// Get the current date and time as a string
+/// </summary>
+/// <param name="format"> Format string, default is [YYYY-MM-DD|HH:MM:SS]</param>
+/// <returns>String of current date and time as the specified format</returns>
+std::string GetCurrentTime(const char* format = "[%Y-%m-%d|%X]");
 
-    //TODO: Get rid of the template. Very unsafe. Using this approach since
-    //getGlobalBounds is defined differently in sf::Shape and sf::Sprite separately.
-    //This will however be fixed inevitably when I create my own ECS for handling 
-    //in-game objects or as it states Entities :)
+// TODO: Get rid of the template. Very unsafe. Using this approach since
+// getGlobalBounds is defined differently in sf::Shape and sf::Sprite separately.
+// This will however be fixed inevitably when I create my own ECS for handling
+// in-game objects or as it states Entities :)
 
-    /// <summary>
-    /// Set the origin/pivot of the object to the center
-    /// </summary>
-    /// <typeparam name="T">Shape or Sprite</typeparam>
-    /// <param name="object">Object with shape/sprite</param>
-    template<typename T>
-    inline void CenterOrigin(T& object)
-    {
-        object.setOrigin(object.getGlobalBounds().getCenter());
-    }
+/// <summary>
+/// Set the origin/pivot of the object to the center
+/// </summary>
+/// <typeparam name="T">Shape or Sprite</typeparam>
+/// <param name="object">Object with shape/sprite</param>
+template <typename T> inline void CenterOrigin(T& object) { object.setOrigin(object.getGlobalBounds().getCenter()); }
 
-    /// <summary>
-    /// Encrypts a given string
-    /// </summary>
-    void Encrypt(std::string& str);
+/// <summary>
+/// Encrypts a given string
+/// </summary>
+void Encrypt(std::string& str);
 
-    /// <summary>
-    /// Decrypt a string which was encrypted by Ren::Encrypt()
-    /// </summary>
-    void Decrypt(std::string& str);
+/// <summary>
+/// Decrypt a string which was encrypted by Ren::Encrypt()
+/// </summary>
+void Decrypt(std::string& str);
 
-    /// <summary>
-    /// Returns the current engine verision as a string
-    /// </summary>
-    /// <returns>std::string</returns>
-    inline std::string GetEngineVersion()
-    {
-        return std::string(std::to_string(REN_VERSION_MAJOR) + "." + std::to_string(REN_VERSION_MINOR) + "." + std::to_string(REN_VERSION_PATCH));
-    }
-}
+/// <summary>
+/// Returns the current engine verision as a string
+/// </summary>
+/// <returns>std::string</returns>
+inline std::string GetEngineVersion() { return std::string(std::to_string(REN_VERSION_MAJOR) + "." + std::to_string(REN_VERSION_MINOR) + "." + std::to_string(REN_VERSION_PATCH)); }
+} // namespace Ren
