30ed6f59e7c1832be7667165a63efd811b429be5
Himanshu Parchand <himan2104@outlook.com>
2025-12-29 20:13:26 +0530

    Pre vacay dump lmao

--- Engine/ECS/Runtime/Include/Rengine/ECS/System.hxx
+++ Engine/ECS/Runtime/Include/Rengine/ECS/System.hxx
@@ -5,7 +5,41 @@ namespace Ren::ECS
 
 class ISystem
 {
+public:
+    friend class World;
+    ISystem()          = default;
+    virtual ~ISystem() = default;
+
 protected:
+    virtual void Start()    = 0;
+    virtual void Shutdown() = 0;
+    virtual void Reset()    = 0;
+    virtual void TickInternal();
+
+    const World* GetWorld() const;
+    World* GetWorld();
+
+private:
+    World* _world{nullptr};
+
+    friend class World;
+};
+
+template <typename... Components>
+class System : public ISystem
+{
+public:
+    template <typename... Cs>
+    class Query;
+
+    System()          = default;
+    virtual ~System() = default;
+
+protected:
+    virtual void Tick(Query<Components...> query) = 0;
+
+private:
+    void TickInternal() override { Tick(); }
 };
 
 } // namespace Ren::ECS
